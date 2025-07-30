93c18c7e9b94c6095af15c1987398db0ddbc0fa7
VenkataChaitanyaKumarPatnala <chaitanyakumar2829@gmail.com>
2025-07-30 10:35:18 +0530

    Convert client from submodule to regular folder

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import MuseumScene from './components/MuseumScene';
+
+function App() {
+  return (
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MuseumScene />
+    </div>
+  );
+}
+
+export default App;
